600204cfa6d0c7bf47ce3c157f8fe59c3a7b5a7f
TORCHE Tony <ttorche@lebatimans.com>
2026-06-10 12:49:57 +0200

    test affichage

--- app/concours/page.tsx
+++ app/concours/page.tsx
@@ -139,7 +139,7 @@ async function chargerConcours() {
         {/* En-tête */}
         <div className="flex justify-between items-center mb-10">
           <h1 className="text-5xl font-bold text-white">
-            🏆 Concours
+            🏆 Concours Bisous
           </h1>
 
           {isAdmin && (
